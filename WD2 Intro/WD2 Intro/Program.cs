9e376acaade317819039bea1f79b54335baee274
j.williams <j.williams@glrealitylabs.com>
2023-11-30 12:45:21 -0500

    Got UI mostly working

--- WD2 Intro/WD2 Intro/Program.cs
+++ WD2 Intro/WD2 Intro/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Globalization;
+using System.IO;
+using System.Media;
+using System.Security.AccessControl;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the command to run and any arguments
+        string command = "cmd.exe";
+        string arguments = "/c echo Hello, World! && timeout /nobreak /t 2";
+
+        // Start the process
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = command,
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            RedirectStandardInput = true,
+            UseShellExecute = false,
+            CreateNoWindow = false // Set this to true if you want to hide the terminal window
+        };
+
+        Process process = new Process { StartInfo = psi };
+
+        process.Start();
+        //Console.ForegroundColor = ConsoleColor.Yellow;
+        //Console.WriteLine("dsuofhwsedufisoh");
+        //Console.ResetColor();
+        //Thread.Sleep(1000);
+        //Console.WriteLine("dfueabgifea");
+
+
+
+
+
+        // You can read the output if needed
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        process.WaitForExit();
+        Console.ReadKey();
+    }
+}
---
+++ WD2IntroNew/WD2IntroNew/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WD2IntroNew
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init_Cont();
+            String Command = ("fdfb:4503:4d58::/48");
+
+            // Subscribe to the PreviewKeyDown event
+            PreviewKeyDown += MainWindow_PreviewKeyDown;
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Handle the key press event and update the TextBox text
+            string currentText = PoweredBy.Text;
+            char pressedKey = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+
+            // Add the pressed key to the existing text
+            Host_Text.Text = currentText + pressedKey;
+
+            // Mark the event as handled to prevent additional processing
+            e.Handled = true;
+        }
+
+        public void Init_Cont()
+        {
+            PoweredBy.IsReadOnly = true;
+            Connect_to_BLUME_Servers___.IsReadOnly = true;
+        }
+    }
+}
