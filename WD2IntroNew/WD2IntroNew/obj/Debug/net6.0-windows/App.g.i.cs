059d0bf4c334187bc1d4d125657b33de9a0fc0ff
Jace <121100026+NizioStudios@users.noreply.github.com>
2024-07-06 20:16:16 -0400

    anim stuff

--- WD2IntroNew/WD2IntroNew/obj/Debug/net6.0-windows/App.g.i.cs
+++ WD2IntroNew/WD2IntroNew/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace WD2IntroNew {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WD2IntroNew;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WD2IntroNew;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WD2IntroNew/WD2IntroNew/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WD2IntroNew/WD2IntroNew/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -205,7 +205,7 @@ namespace WD2IntroNew {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WD2IntroNew;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WD2IntroNew;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
